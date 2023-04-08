f10e467171f908b9df93b152cca406e362efa1ab
PATY <PATY@LAPTOP-UKLUO7EK>
2023-04-07 21:37:26 -0300

    Adicionar arquivos de projeto.

---
+++ Rec. acessadores e Classe/ClienteBanco.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rec.acessadores_e_Classe
+{
+    class ClienteBanco
+    {
+        private int Conta { get;  set; }
+        public string Nome  { get; set; }
+
+        public double ValorInicialdeDeposito { get; set; }
+
+
+        public ClienteBanco()
+        {
+
+        }
+
+        public ClienteBanco(int conta, string nome, double valorinicialdedeposito)
+        {
+            Conta += conta;
+            Nome += nome;
+            ValorInicialdeDeposito += valorinicialdedeposito;
+        }
+
+        
+
+        public void AumentarValordeDeposito()
+        {
+             ValorInicialdeDeposito += ValorInicialdeDeposito + 200.00;
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Dados da Conta:" +
+                " Conta:" + Conta +
+                " Nome: " + Nome +
+                " Valor do Deposito:  R$ " + ValorInicialdeDeposito.ToString("f2", CultureInfo.InvariantCulture) + " Reais)";
+        }
+
+
+
+
+    }
+
+}
---
+++ Rec. acessadores e Classe/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rec.acessadores_e_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+            ClienteBanco conta;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Hávera depósito inicial? S/N: ");
+            char resposta = char.Parse(Console.ReadLine());
+
+            
+
+
+            
+            
+
+
+            if (resposta== 'S')
+
+
+            {
+                Console.WriteLine("Entre com o Valor inicial do Deposíto: ");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("Valor do Deposito:  R$  " + saldo .ToString("F2", CultureInfo.InvariantCulture)  + " Reais ");
+                Console.WriteLine();
+              
+
+                conta = new ClienteBanco(numero,nome,saldo);
+
+                Console.WriteLine("Dados da Conta: " + conta);
+
+            }
+
+            else 
+            {
+                conta = new ClienteBanco(numero, nome);
+
+                Console.WriteLine(conta);
+
+            }
+
+
+            Console.Write("Entre com outro valor: ");
+            double deposito = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com o Valor do Saque: ");
+            double saque = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            conta.Saque(saque);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados:");
+            Console.WriteLine(conta);
+
+            Console.ReadKey();
+
+            
+            
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+            
+            
+            
+            
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
